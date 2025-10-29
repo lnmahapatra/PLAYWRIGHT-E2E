aced622bc421ee57c6732c0da45e33d988528fe7
lnmahapatra <68851434+lnmahapatra@users.noreply.github.com>
2025-10-29 22:18:31 +0530

    Change test directory to 'e2e/src/tests'

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: 'src/tests',
+  testDir: 'e2e/src/tests',
   timeout: 60_000,
   expect: { timeout: 5000 },
   /* Run tests in files in parallel */
